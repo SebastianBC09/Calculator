0a681ec062d7f93041af486ee5bdcaafcdff5026
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-03-11 17:01:05 -0500

    BMI Calculator using node & express added to the project. I'll add CSS styles & more design in the near future.

--- calculator.js
+++ calculator.js
@@ -13,13 +13,24 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html")
 })
 
-app.post("/", function(req, res){
-  var num1 = Number(req.body.num1)
-  var num2 = Number(req.body.num2)
-  
-  var result = num1 + num2
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html")
+})
 
+app.post("/", function(req, res){
+  var num1 = Number(req.body.n1)
+  var num2 = Number(req.body.n2)
+  var result = num1 + num2
   res.send("The result of the calculation is: " + result)
+})
+
+app.post("/bmicalculator", function(req, res){
+  var height = parseFloat(req.body.height)
+  var weight = parseFloat(req.body.weight)
+  var bmi = weight / Math.pow(height, 2)
+  bmi = bmi.toFixed(2)
+
+  res.send("Your BMI is: " + bmi)  
 })
 
 app.listen(port, function(){
